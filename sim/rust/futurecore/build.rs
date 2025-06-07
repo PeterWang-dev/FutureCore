0220ad2be368196a01a5528564814d82c37fdcbd
PeterWang-dev <PeterWang030908@hotmail.com>
2025-06-07 22:58:22 +0800

    build(build.rs): coherent build script with cargo profile and cmake of verilated_cpp

--- sim/rust/futurecore/build.rs
+++ sim/rust/futurecore/build.rs
@@ -5,13 +5,13 @@ use std::{env, path::PathBuf};
 fn main() {
     let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
 
-    let verilated_cpp_dir = manifest_dir.join("verilated_cpp");
-    let vsrc_path = verilated_cpp_dir.join("vsrc");
-    println!("cargo:rerun-if-changed={}", vsrc_path.display());
+    let verilated_cpp = manifest_dir.join("verilated_cpp");
+    println!("cargo:rerun-if-changed={}", verilated_cpp.display());
 
-    let cmake_out_dir = cmake::build(&verilated_cpp_dir);
+    let cmake_out_dir = cmake::Config::new(&verilated_cpp).build();
     let verilator =
         pkg_config::probe_library("verilator").expect("Failed to find verilator via pkg-config");
+
     // Not really do compile actually, as we are using cmake to build the verilated_cpp.
     // Here we just "compile" the libverilated_cpp.a to libverilated.a
     cxx_build::bridge("src/core/sim/verilated.rs")
@@ -20,11 +20,10 @@ fn main() {
         .includes(verilator.include_paths)
         .cpp(true)
         .std("c++20")
-        .opt_level(3)
+        .warnings(false)
         .compile("verilated");
 
     println!("cargo:rustc-link-search=native={}", cmake_out_dir.display());
-    println!("{}", cmake_out_dir.display());
     println!("cargo:rustc-link-lib=static=verilated_cpp");
     println!("cargo:rustc-link-lib=z");
 }
