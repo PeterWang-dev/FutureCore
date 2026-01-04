4e1ff7a677ff870f29528b828a8c85555642e7dc
PeterWang-dev <PeterWang030908@hotmail.com>
2026-01-05 03:13:20 +0800

    chore: add fenix overlay

--- shell.nix
+++ shell.nix
@@ -1,18 +1,28 @@
 {
-  pkgs ? import (fetchTarball "https://nixos.org/channels/nixos-25.11/nixexprs.tar.xz") { },
+  pkgs ? import (fetchTarball "https://nixos.org/channels/nixos-25.11/nixexprs.tar.xz") {
+    overlays = [
+      (import "${fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz"}/overlay.nix")
+    ];
+  },
+
 }:
 let
   # use mill 0.11.12 instead of the latest version with breaking changes
   mill_0_11 = pkgs.callPackage ./nix/mill-0.11.12.nix { };
+  rustToolchain = pkgs.fenix.fromToolchainFile {
+    file = ./sim/rust/rust-toolchain.toml;
+    sha256 = "sha256-sqSWJDUxc+zaz1nBWMAJKTAGBuGWP25GCftIOlCEAtA=";
+  };
   npc = pkgs.callPackage ./default.nix { inherit mill_0_11; };
 in
 pkgs.mkShell rec {
   name = "npc-env";
   inputsFrom = [ npc ];
   packages = with pkgs; [
+    rustToolchain
+    metals
     gtkwave
     ieda
-    metals
   ];
   shellHook = ''
     export NPC_HOME=${NPC_HOME}
