185f74e84f9c2f081631d02cd2aa2b83e25720dd
PeterWang-dev <PeterWang030908@hotmail.com>
2026-03-13 20:37:31 +0800

    feat(hw/misc/TypeExtensions): add zero extend extension

--- hw/spinal/futurecore/misc/TypeExtensions.scala
+++ hw/spinal/futurecore/misc/TypeExtensions.scala
@@ -6,4 +6,8 @@ object TypeExtensions {
   implicit class BitsSextExtension(b: Bits) {
     def sext: SInt = b.asSInt.resize(32 bits)
   }
+
+  implicit class BitsZeroExtension(b: Bits) {
+    def zext: UInt = b.asUInt.resize(32 bits)
+  }
 }
