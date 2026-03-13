070a461019a40479f273ebc80c3890c76bf10af0
PeterWang-dev <PeterWang030908@hotmail.com>
2026-03-13 20:51:04 +0800

    feat(hw/decode/ImmGenerator): add micro imm of csr

--- hw/spinal/futurecore/decode/ImmGenerator.scala
+++ hw/spinal/futurecore/decode/ImmGenerator.scala
@@ -2,12 +2,13 @@ package futurecore.decode
 
 import spinal.core._
 
-import futurecore.riscv.Rvi.{IImm, SImm, BImm, UImm, JImm}
-import futurecore.misc.TypeExtensions.BitsSextExtension
+import futurecore.riscv.Rv32i.{IImm, SImm, BImm, UImm, JImm}
+import futurecore.riscv.Zicsr.{MicroImm => CsrMicroImm}
+import futurecore.misc.TypeExtensions.{BitsSextExtension, BitsZeroExtension}
 
 object ImmGenerator {
   object ImmMode extends SpinalEnum {
-    val I, S, B, U, J = newElement()
+    val I, S, B, U, J, CsrU = newElement()
   }
 }
 
@@ -28,12 +29,18 @@ class ImmGenerator extends Component {
   val uImm = (UImm.extract(inst) ## U"12'b0").sext
   val jImm = (JImm.extract(inst) ## U"1'b0").sext
 
+  val zicsrMicroImm = (CsrMicroImm.extract(inst)).zext
+
   // Select immediate based on mode
   io.outImm := io.inSelMode.mux(
     ImmMode.I -> iImm,
     ImmMode.S -> sImm,
     ImmMode.B -> bImm,
     ImmMode.U -> uImm,
-    ImmMode.J -> jImm
+    ImmMode.J -> jImm,
+    // Although uImm is UInt and zero-extended,to make type system happy,
+    // still needs to convert to SInt. This will not cause any problem as
+    // binary representation keep untouched.
+    ImmMode.CsrU -> zicsrMicroImm.asSInt
   )
 }
